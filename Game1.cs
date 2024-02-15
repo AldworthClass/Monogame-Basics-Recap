9bff5364c72208426aced72b510b1174f2c1e44d
Steve Aldworth <s.aldworth@tvdsb.ca>
2024-02-15 12:47:13 -0500

    Added resources and draw with rotation.

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace Importing_Scaling_and_Randomization
 {
@@ -8,6 +10,20 @@ namespace Importing_Scaling_and_Randomization
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Random generator = new Random();
+
+        Texture2D backgroundTexture;
+        Rectangle backgroundRect;
+
+        Texture2D shipTexture;
+        Rectangle shipRect;
+
+        List<Texture2D> shipTextures;
+
+        SpriteFont titleFont;
+
+        float angle;
 
         public Game1()
         {
@@ -18,7 +34,19 @@ namespace Importing_Scaling_and_Randomization
 
         protected override void Initialize()
         {
+
             // TODO: Add your initialization logic here
+            Window.Title = "Content, Scaling and Text";
+
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
+
+            shipRect = new Rectangle(generator.Next(_graphics.PreferredBackBufferWidth - 75), generator.Next(_graphics.PreferredBackBufferHeight - 75), 75, 100);
+            backgroundRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            
+            // Calculates random angle from 0-360 to rotate ship
+            angle = 2 * MathF.PI * (float)generator.NextDouble();
 
             base.Initialize();
         }
@@ -28,6 +56,12 @@ namespace Importing_Scaling_and_Randomization
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            titleFont = Content.Load<SpriteFont>("Fonts/TitleFont");
+            backgroundTexture = Content.Load<Texture2D>("Images/space_background");
+            shipTextures = new List<Texture2D>();
+            for (int i = 1; i <= 5; i++)
+                shipTextures.Add(Content.Load<Texture2D>("Images/enterprise_" + i));
+            shipTexture = shipTextures[generator.Next(shipTextures.Count)];
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +79,14 @@ namespace Importing_Scaling_and_Randomization
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(backgroundTexture, backgroundRect, Color.White);
+            _spriteBatch.DrawString(titleFont, "Space", new Vector2(300, 10), Color.Yellow);
+            _spriteBatch.Draw(shipTexture, shipRect, Color.White);
+            _spriteBatch.Draw(shipTexture, shipRect, null, Color.White, angle, new Vector2(shipTexture.Width / 2, shipTexture.Height / 2), SpriteEffects.None, 1f);
+            
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
